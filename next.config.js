34dc9dd5e99a39f3b1bb61b647396cf12669863f
yiyeop <syl0326@gmail.com>
2021-09-15 15:39:22 +0900

    nextjs, emotion, tailwind initial setting

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// next.config.js
+module.exports = {
+  webpack5: true,
+  webpack: (config) => {
+    // Unset client-side javascript that only works server-side
+    config.resolve.fallback = { fs: false, module: false };
+
+    return config;
+  },
+};
---
+++ src/pages/_app.js
@@ -0,0 +1,12 @@
+import { GlobalStyles } from 'twin.macro';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
